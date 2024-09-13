cb00413aedfc96e1339818de9602a18a4b45bad6
zhangll <zhangll@tigersoft.co.jp>
2024-09-13 18:33:41 +0900

    0913-TODO logic

---
+++ src/app/calculator.ts
@@ -0,0 +1,10 @@
+export interface Hero {
+  displayNumberA: number; // 当前输入的数字
+  calculateNumberB: number; // 上一个输入的数字
+  storageNumberC: number; // 存储的数字
+  operation: number; // 当前操作符
+  hasCalculated: boolean;
+}
+
+
+
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
+  template: `<h1>hhhhhhhhhhhhHello world!</h1>`,
 })
 export class CalculatorComponent {
-  answer = 'Windstorm';
+  displayNumberA = ''; // 当前输入的数字
+  calculateNumberB = ''; // 上一个输入的数字
+  storageNumberC = ''; // 存储的数字
+  operation = ''; // 当前操作符
+  hasCalculated = false;
+
+
+  removeAll() {
+    this.displayNumberA = '';
+    this.calculateNumberB = '';
+    this.storageNumberC = '';
+    this.operation = '';
+    this.hasCalculated = false;
+    this.updateDisplay();
+  }
+
+
+  updateDisplay(): void {
+    const displayElement = document.getElementById('display') as HTMLInputElement | null;
+    if (displayElement) {
+      displayElement.value = this.displayNumberA; //TODO 要不要转字符串String(this.displayNumber)
+    } else {
+      console.error("Element with ID 'display' not found.");
+    }
+  }
+
+  a(number: number): void {
+    this.displayNumberA += number;
+    this.updateDisplay();
+  }
+
+  setOperation(operation: string): void {
+
+
+  }
+
+  //@Input() housingLocation!: HousingLocation;
+
 }
